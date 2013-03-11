44d6e053bf0207246ae25d3d7c4f7737eb554dda
Christopher M Miles <twitch@nervestaple.com>
2013-03-11 08:43:31 -0400

    Worked on Selenium tests.

--- src/test/java/com/windsorsolutions/todos/entities/ToDoTest.java
+++ src/test/java/com/windsorsolutions/todos/entities/ToDoTest.java
@@ -1,6 +1,7 @@
 package com.windsorsolutions.todos.entities;
 
 import org.junit.*;
+
 import java.util.Date;
 
 public class ToDoTest {
@@ -12,48 +13,48 @@ public class ToDoTest {
     @Before
     public void setUp() {
 
-	context = new Context();
-	context.setId(new Long(101));
+        context = new Context();
+        context.setId(new Long(101));
 
-	todo = new ToDo();
-	todo.setId(new Long(201));
-	todo.setContext(context);
-	todo.setContent("This is my todo item.");
-	todo.setCompletedAt(completedAtDate);
+        todo = new ToDo();
+        todo.setId(new Long(201));
+        todo.setContext(context);
+        todo.setContent("This is my todo item.");
+        todo.setCompletedAt(completedAtDate);
     }
 
     @Test
     public void testId() {
 
-	Long expected = new Long(201);
-	Long result = todo.getId();
-	Assert.assertEquals(expected, result);
+        Long expected = new Long(201);
+        Long result = todo.getId();
+        Assert.assertEquals(expected, result);
     }
 
     @Test
     public void testContext() {
 
-	Context expected = context;
-	Context result = todo.getContext();
+        Context expected = context;
+        Context result = todo.getContext();
 
-	Assert.assertEquals(expected, result);
+        Assert.assertEquals(expected, result);
     }
 
     @Test
     public void testContent() {
 
-	String expected = "This is my todo item.";
-	String result = todo.getContent();
+        String expected = "This is my todo item.";
+        String result = todo.getContent();
 
-	Assert.assertEquals(expected, result);
+        Assert.assertEquals(expected, result);
     }
 
     @Test
     public void testCompletedAt() {
 
-	Date expected = completedAtDate;
-	Date result = todo.getCompletedAt();
+        Date expected = completedAtDate;
+        Date result = todo.getCompletedAt();
 
-	Assert.assertEquals(expected, result);
+        Assert.assertEquals(expected, result);
     }
 }
--- src/test/java/com/windsorsolutions/todos/integration/service/ToDoHomePageTest.java
+++ src/test/java/com/windsorsolutions/todos/integration/service/ToDoHomePageTest.java
@@ -1,10 +1,22 @@
 package com.windsorsolutions.todos.integration.service;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.*;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
 
 public class ToDoHomePageTest {
 
@@ -13,4 +25,60 @@ public class ToDoHomePageTest {
      */
     private Logger logger = LoggerFactory.getLogger(ToDoHomePageTest.class);
 
+    /**
+     * Selenium web driver instance.
+     */
+    private WebDriver driver = null;
+
+    @Before
+    public void setUp() {
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        driver = new PhantomJSDriver(desiredCapabilities);
+        driver.manage().window().setSize(new Dimension(800, 600));
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testHomePageRenders() {
+
+        driver.get("http://localhost:51982/");
+
+        Assert.assertNotSame(driver.findElements(By.id("title")).size(), 0);
+        Assert.assertNotSame(driver.findElements(By.id("addTodoLink")).size(), 0);
+        Assert.assertNotSame(driver.findElements(By.id("addContextLink")).size(), 0);
+    }
+
+    @Test
+    public void textAddContext() {
+
+        driver.get("http://localhost:51982/");
+        driver.findElement(By.id("addContextLink")).click();
+
+        WebElement webElementModal = driver.findElement(By.className("wicket_modal"));
+        logger.info("Modal: " + webElementModal + ", " + webElementModal.getAttribute("id"));
+
+        List<WebElement> elements = driver.findElements(By.tagName("iframe"));
+        for(WebElement webElement : elements) {
+            logger.info("  Iframe: " + webElement + ", " + webElement.getAttribute("id"));
+
+            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            try {
+                FileUtils.copyFile(screenshot, new File("screenshot.png"));
+            } catch (IOException e2) {
+                logger.info("Couldn't save screenshot: " + e2.getMessage());
+            }
+
+            driver.switchTo().frame(webElement);
+        }
+
+        driver.findElement(By.id("contextAddNameField"));
+
+        Assert.assertNotSame(driver.findElements(By.className("wicket-modal")).size(), 0);
+    }
+
+    @After
+    public void breakDown() {
+        driver.quit();
+    }
 }
